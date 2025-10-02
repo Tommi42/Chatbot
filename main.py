fce34111c8724ed990c2d0ea2550407fa15bcad7
Tommi <t.cambursano@gmail.com>
2025-10-02 13:58:06 +0200

    langchain

--- main.py
+++ main.py
@@ -1,25 +1,48 @@
+from langchain_core.messages.utils import MessageLikeRepresentation
+from openai.types.chat.completion_create_params import ResponseFormat
 import streamlit as st
 from openai_client import get_response
+from openai_client import message_manager, init_chat_model_langchain, display_message
+from openai_client import Role
 import os
 
-if 'message_list' not in st.session_state:
-    st.session_state.message_list = []
+if "chat_model" not in st.session_state or "message_list" not in st.session_state:
+    init_chat_model_langchain()
 
-st.title("Chatbot")
+st.set_page_config(page_title="Chatbot", layout="wide")
+
+c1, c2 = st.columns([5, 1])
 
 input = st.chat_input("Type your message here")
 
-for message in st.session_state.message_list:
-    if message['role'] == 'user':
-        st.chat_message("user").markdown(message['content'])
-    elif message['role'] == 'assistant':
-        st.chat_message("assistant").markdown(message['content'])
 
-if input:
-    with st.chat_message("user"):
-        st.write(input)
-    st.session_state.message_list.append({"role": "user", "content": input})
-    response = get_response()
-    with st.chat_message("assistant"):
-        response_txt = st.write_stream(response)
-    st.session_state.message_list.append({"role": "assistant", "content": response_txt})
+with c2:
+    st.subheader("About")
+    st.text(
+        "This is a Demo Chatbot built using Streamlit as UI and langchain as backend to manage LLM."
+    )
+    st.markdown("This project was built from ME, *Tommaso Cambursano* :)")
+    st.text(
+        "It is just a demo to demonstrate my knoledge on Langchain and LLM managing. You can directly ask the chatbot about me and me previuos project."
+    )
+
+    st.button("New chat", on_click=lambda: st.session_state.message_list.clear())
+
+
+with c1:
+    st.title("Chatbot")
+
+    for message in st.session_state.message_list:
+        display_message(message)
+
+    if input:
+        with st.chat_message("user"):
+            st.write(input)
+        message_manager(Role("user"), input)
+
+        response = get_response()
+
+        with st.chat_message("assistant"):
+            response_txt = st.write_stream(response)
+
+        message_manager(Role("assistant"), str(response_txt))
--- openai_client.py
+++ openai_client.py
@@ -1,13 +1,63 @@
+from socket import AI_ADDRCONFIG
+from sqlalchemy.sql.roles import ReturnsRowsRole
 import streamlit as st
 import openai
 from dotenv import load_dotenv
+from enum import Enum
+from langchain.chat_models import init_chat_model
+from langchain_core.messages import HumanMessage, SystemMessage, AIMessage
 
 load_dotenv()
 
+
+class Role(Enum):
+    USER = "user"
+    SYSTEM = "system"
+    ASSISTANT = "assistant"
+
+
 def get_response():
-    stream = openai.chat.completions.create(
-        model="gpt-4o-mini",
-        messages= st.session_state.message_list,
+    chat_model = st.session_state.chat_model
+
+    stream = chat_model.stream(
+        input=st.session_state.message_list,
         stream=True,
     )
     return stream
+
+
+def init_chat_model_langchain():
+    try:
+        prompt = open("initial_prompt.txt", "r").read()
+    except FileNotFoundError:
+        prompt = ""
+    st.session_state.message_list = [SystemMessage(content=prompt)]
+
+    st.session_state.chat_model = init_chat_model(
+        "gpt-4o", model_provider="openai", temperature=0
+    )
+
+    return True
+
+
+def message_manager(role: Role, content: str):
+    if role == Role.USER:
+        message = HumanMessage(content=content)
+    elif role == Role.SYSTEM:
+        message = SystemMessage(content=content)
+    elif role == Role.ASSISTANT:
+        message = AIMessage(content=content)
+    else:
+        raise ValueError("Invalid role")
+
+    st.session_state.message_list.append(message)
+    return message
+
+
+def display_message(message):
+    if isinstance(message, HumanMessage):
+        st.chat_message("user").markdown(message.content)
+    elif isinstance(message, AIMessage):
+        st.chat_message("assistant").markdown(message.content)
+    else:
+        pass  # Do not diplay anaything else
